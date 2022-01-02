ffcf23bf86f4ec3117f5cecfade376082ac81d2f
Operator873 <49728480+Operator873@users.noreply.github.com>
2022-01-01 23:41:10 -0600

    Correct URL for normal installs. Create a clickable link

--- send-service.py
+++ send-service.py
@@ -5,6 +5,7 @@ from discord_webhook import DiscordWebhook, DiscordEmbed
 
 HOOK = "https://discordapp.com/api/webhooks/id/token"
 KEYS = ['type', 'servdesc', 'host', 'hostaddr', 'servstate', 'date', 'output']
+DOMAIN = "your.website.com"
 
 def codecolor(type):
     clr_red = 13632027
@@ -21,14 +22,17 @@ def codecolor(type):
 def main(nagIn):
     cmd = nagIn.pop(0)
     data = {KEYS[i]: nagIn[i] for i in range(len(KEYS))}
+
+    link = "https://" + DOMAIN + "/nagios/cgi-bin/extinfo.cgi?type=2&host=" + data['host'] + "&service=" + data['servdesc']
     if data['hostaddr'] == "127.0.0.1":
         data['hostaddr'] = "10.10.3.5"
-    line1 = "***" + data['type'] + "*** " + data['host'] + " - " + data['servdesc'] + ": " + data['servstate']
+    line1 = "**<" + data['type'] + ">** " + data['host'] + " - " + data['servdesc'] + ": " + data['servstate']
     line2 = data['hostaddr'] + " " + data['output']
 
     webhook = DiscordWebhook(url=HOOK)
     # create embed object for webhook
     embed = DiscordEmbed(title=line1, description=line2, color=codecolor(data['type']))
+    embed.set_author(name='Open Nagios service detail', url=link)
 
     # set timestamp
     embed.set_timestamp(int(data['date']))
